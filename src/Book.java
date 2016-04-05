cce63dd3d0a65802a7164b8a19c70bda3e00c6b0
Dale Micallef <bakzi95@gmail.com>
2016-04-05 13:19:20 +0200

    Trying out tests

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,23 @@
 /**
- * Created by Dale on 05/04/2016.
+ * Created by Dale & Romario on 05/04/2016.
  */
 public class Book {
+    String title;
+    String author;
+    String genre;
+    int yearofpublication;
+    int edition;
+
+    public Book(String title, String author, String genre, int yearofpublication, int edition) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.yearofpublication = yearofpublication;
+        this.edition = edition;
+    }
+    int method{
+        return 0
+    }
+
 }
+
---
+++ src/test/BookTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dale on 05/04/2016.
+ */
+public class BookTest {
+
+    @Test
+    public void checkingTests() {
+        assertTrue(true);
+    }
+
+}
