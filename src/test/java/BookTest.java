487199ce63193f2d6134cfefcd2e515007a35416
Romario Catania <romariocatania84@gmail.com>
2016-04-06 21:35:40 +0200

    Emma has been fixed locally (problem was: there was no test coverage at all!).  Now it shall be tested on Jenkins

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,13 +1,14 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 
 public class BookTest {
 
 	@Test
-	public void test() {
-		assertTrue(false);
+		public void test() {
+		Book bk = new Book("", "", "", 2, 3);
+		assertTrue(bk != null);
 	}
+	
 
 }
