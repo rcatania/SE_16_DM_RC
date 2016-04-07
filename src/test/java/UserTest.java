462ef89b70f7b4eb3fd52ad4c62d2582c2a381b7
Romario Catania <romariocatania84@gmail.com>
2016-04-07 08:10:05 +0200

    Adding the test for testing User IDs

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 public class UserTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void idTest() {
+		User u1 = new User("abc");
+		User u2 = new User("cdf");
+		User u3 = new User("efg");
+		
+		//checking they are unique
+		assertTrue((u1.getId() != u2.getId()) && (u1.getId() != u3.getId()) && (u2.getId() != u3.getId()));
 	}
-
-	
 }
