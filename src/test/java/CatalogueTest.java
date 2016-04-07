31ca798f1e75fb65deccefe99d747474bf8f0dff
Romario Catania <romariocatania84@gmail.com>
2016-04-07 08:49:56 +0200

    Created CatalogueTest class

---
+++ src/test/java/CatalogueTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CatalogueTest {
+
+	@Test
+	public void testGetAllBooks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchByTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchByGenre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchByYearOfPublication() {
+		fail("Not yet implemented");
+	}
+
+}
