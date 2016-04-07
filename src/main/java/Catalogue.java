71eb7d79dc3993f1337f47f6cb6219cc306f07d9
Romario Catania <romariocatania84@gmail.com>
2016-04-07 08:46:12 +0200

    Created a new class (Catalogue) with skeleton code.

---
+++ src/main/java/Catalogue.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+
+public class Catalogue {
+	
+	public ArrayList<Book> getAllBooks() {
+		return null;
+	}
+	
+	public ArrayList<Book> searchByTitle(String title) {
+		return null;
+	}
+	
+	public ArrayList<Book> searchByGenre(Genre g) {
+		return null;
+	}
+	
+	public ArrayList<Book> searchByYearOfPublication(int year) {
+		return null;
+	}
+}
