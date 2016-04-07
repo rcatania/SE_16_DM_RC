5c648dd93544f79187432b89e315a45178de450b
Romario Catania <romariocatania84@gmail.com>
2016-04-07 11:11:34 +0200

    Written skeleton code for the Library class, just enough to be able to write tests.

---
+++ src/main/java/Library.java
@@ -0,0 +1,18 @@
+
+public class Library {
+	public void addUser(User u) {
+		
+	}
+	
+	public void removeUser(User u) {
+		
+	}
+	
+	public int numberOfUsers() {
+		return 0;
+	}
+	
+	public boolean isUserRegistered(User u) {
+		return false;
+	}
+}
