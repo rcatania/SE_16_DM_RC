cd99009b2c2998a7957803644f7c715c26393ac6
Romario Catania <romariocatania84@gmail.com>
2016-04-07 08:33:04 +0200

    Added getters and setters tests

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -18,7 +18,26 @@ public class BookTest {
 		Book bk3 = new Book("", "", "", 2, 3);
 
 		assertTrue((bk1.getId() != bk2.getId()) && (bk1.getId() != bk3.getId()) && (bk2.getId() != bk3.getId()));
+	}
+	
+	@Test
+	public void gettersSetters() {
+		Book bk1 = new Book("", "", "", 2, 3);
 
+		bk1.setAuthor("Anne Frank");
+		assertTrue(bk1.getAuthor().equals("Anne Frank"));
+		
+		bk1.setTitle("Diary");
+		assertTrue(bk1.getTitle().equals("Diary"));
+		
+		bk1.setGenre("Nonfiction");
+		assertTrue(bk1.getGenre().equals("Nonfiction"));
+
+		bk1.setYearofpublication(1945);
+		assertTrue(bk1.getYearofpublication() == 1945);
+		
+		bk1.setEdition(3);
+		assertTrue(bk1.getEdition() == 3);
 	}
 	
 
