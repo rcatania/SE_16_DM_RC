992b4269eae9223d50baa3454649fa62778e0322
Romario Catania <romariocatania84@gmail.com>
2016-05-09 08:03:03 +0200

    Removed all the searchBy methods (should have been done in previous commit).

--- src/main/java/Catalogue.java
+++ src/main/java/Catalogue.java
@@ -23,38 +23,6 @@ public class Catalogue {
 		return newLst;
 	}
 	
-	public ArrayList<Book> searchByTitle(String title) {
-		ArrayList<Book> newLst = new ArrayList<Book>();
-		String lc_title = title.toLowerCase();
-		for (Book bk : cat) {
-			String bookTitle = bk.getTitle().toLowerCase();
-			if (bookTitle != null && bookTitle.contains(lc_title))
-				newLst.add(bk);
-		}
-		
-		return newLst;
-	}
-	
-	public ArrayList<Book> searchByGenre(Genre g) {
-		ArrayList<Book> newLst = new ArrayList<Book>();
-		for (Book bk : cat) {
-			if (bk != null && bk.getGenre() == g)
-				newLst.add(bk);
-		}
-		
-		return newLst;	
-	}
-	
-	public ArrayList<Book> searchByYearOfPublication(int year) {
-		ArrayList<Book> newLst = new ArrayList<Book>();
-		for (Book bk : cat) {
-			if (bk.getYearofpublication() == year)
-				newLst.add(bk);
-		}
-		
-		return newLst;	
-	}
-	
 	public ArrayList<Book> searchForBooks(Filter f) {
 		ArrayList<Book> updated = new ArrayList<Book>();
 		for (Book b : cat) {
