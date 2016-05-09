3e162388d60c3f6ba26ec8f0fcaf90527a647a33
Romario Catania <romariocatania84@gmail.com>
2016-05-09 07:46:12 +0200

    Writing more code for the filter

--- src/main/java/Filter.java
+++ src/main/java/Filter.java
@@ -15,4 +15,29 @@ public class Filter {
 		childfilters.add(f);
 	}
 	
+	public boolean checkBookFits(Book b) {
+		boolean condition = false;
+		switch(searchfield) {
+		case GENRE:{
+			condition = b.getGenre() == Genre.valueOf(argument);
+		}
+		case TITLE : {
+			String bookTitle = b.getTitle().toLowerCase();
+			if (bookTitle != null && bookTitle.contains(argument))
+				condition = true;
+		}
+		case YEAR_OF_PUBLICATION: {
+			int year = Integer.parseInt(argument);
+			condition = year == b.getYearofpublication();
+		}
+		}
+		
+		for (Filter f : childfilters) {
+			if (!condition) 
+				break;
+			condition &= f.checkBookFits(b);
+		}
+		
+		return condition;
+	}
 }
